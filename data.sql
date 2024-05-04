b9b4bb8b694468220973c7aa98f0fa31443091c8
shivadharMSU <shivadharreddy1995@gmail.com>
2024-05-04 12:25:00 -0400

    tables data

--- data.sql
+++ data.sql
@@ -1,31 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Apr 29, 2024 at 06:11 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.1.17
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `funky_panda_merch`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cart`
---
 
 CREATE TABLE `cart` (
   `cart_id` int(11) NOT NULL,
@@ -50,11 +30,11 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`category_id`, `category_name`) VALUES
-(1, 'category1'),
-(2, 'category2'),
-(3, 'category3'),
-(4, 'category4'),
-(5, 'category5');
+(1, 'Apparel'),
+(2, 'Plushies'),
+(3, 'collectables'),
+(4, 'Accessories'),
+(5, 'Home Decor');
 
 -- --------------------------------------------------------
 
@@ -78,8 +58,8 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`customer_id`, `first_name`, `last_name`, `mobile`, `email`, `address`, `age`, `gender`) VALUES
-(1, 'shivadhar', 'arikonham', '789768', 'shivadhar@gmail.com', '167 col hbewd', 22, 'male'),
-(2, 'Alice', 'bob', '8765789876', 'alice@gmail.com', 'jersey coty', 22, 'male');
+(1, 'snehitha', 'merlin', '789768', 'snehitha@gmail.com', '167 col hbewd', 23, 'female'),
+(2, 'rohan', 'bob', '8765789876', 'rohan@gmail.com', 'jersey coty', 22, 'male');
 
 -- --------------------------------------------------------
 
@@ -121,7 +101,7 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`employee_id`, `name`, `mobile`, `email`, `designation_id`, `salary`, `manager_id`) VALUES
-(1, 'admin', '787657866', 'admin@gmail.com', 1, 22000, NULL);
+(1, 'chandhu', '787657866', 'admin@gmail.com', 1, 22000, '1');
 
 -- --------------------------------------------------------
 
@@ -142,9 +122,9 @@ CREATE TABLE `login_details` (
 --
 
 INSERT INTO `login_details` (`login_deatils_id`, `user_name`, `password`, `login_user_type`, `login_user_id`) VALUES
-(1, 'shivadhar', 'shivapassword', 'customer', 1),
-(2, 'alice', 'alicepassword', 'customer', 2),
-(3, 'admin', 'adminpassword', 'employee', 1);
+(1, 'snehitha', 'shehithapassword', 'customer', 1),
+(2, 'rohan', 'rohanpassword', 'customer', 2),
+(3, 'change', 'chandupassword', 'employee', 1);
 
 -- --------------------------------------------------------
 
@@ -155,7 +135,7 @@ INSERT INTO `login_details` (`login_deatils_id`, `user_name`, `password`, `login
 CREATE TABLE `product` (
   `product_id` int(11) NOT NULL,
   `name` varchar(100) DEFAULT NULL,
-  `description` varchar(100) DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
   `color` varchar(100) DEFAULT NULL,
   `price` decimal(10,0) DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL,
@@ -167,37 +147,38 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_id`, `name`, `description`, `color`, `price`, `quantity`, `category_id`) VALUES
-(1, 'cat 1 product_One', 'description of product one', 'color-one', 44, 10, 1),
-(2, 'cat 1 product_two', 'description of product one', 'color-one', 44, 10, 1),
-(3, 'cat 1 product_three', 'description of product one', 'color-one', 44, 10, 1),
-(4, 'cat 1 product_four', 'description of product one', 'color-one', 44, 10, 1),
-(5, 'cat 1 product_five', 'description of product one', 'color-one', 44, 10, 1),
-(6, 'cat 2 product_One', 'description of product one', 'color-one', 44, 10, 2),
-(7, 'cat 2 product_two', 'description of product one', 'color-one', 44, 10, 2),
-(8, 'cat 2 product_three', 'description of product one', 'color-one', 44, 10, 2),
-(9, 'cat 2 product_four', 'description of product one', 'color-one', 44, 10, 2),
-(10, 'cat 2 product_five', 'description of product one', 'color-one', 44, 10, 2),
-(11, 'cat 3 product_One', 'description of product one', 'color-one', 44, 10, 3),
-(12, 'cat 3 product_two', 'description of product one', 'color-one', 44, 10, 3),
-(13, 'cat 3 product_three', 'description of product one', 'color-one', 44, 10, 3),
-(14, 'cat 3 product_four', 'description of product one', 'color-one', 44, 10, 3),
-(15, 'cat 3 product_five', 'description of product one', 'color-one', 44, 10, 3),
-(16, 'cat 4 product_One', 'description of product one', 'color-one', 44, 10, 4),
-(17, 'cat 4 product_two', 'description of product one', 'color-one', 44, 10, 4),
-(18, 'cat 4 product_three', 'description of product one', 'color-one', 44, 10, 4),
-(19, 'cat 4 product_four', 'description of product one', 'color-one', 44, 10, 4),
-(20, 'cat 4 product_five', 'description of product one', 'color-one', 44, 10, 4),
-(21, 'cat 5 product_One', 'description of product one', 'color-one', 44, 10, 5),
-(22, 'cat 5 product_two', 'description of product one', 'color-one', 44, 10, 5),
-(23, 'cat 5 product_three', 'description of product one', 'color-one', 44, 10, 5),
-(24, 'cat 5 product_four', 'description of product one', 'color-one', 44, 10, 5),
-(25, 'cat 5 product_five', 'description of product one', 'color-one', 44, 10, 5);
+(1, 'Kung Fu Panda 3 Po T-shirt', 'Kung Fu Panda 3 Po Partrait Movie Poster Premium cotton T-shirt', 'Black', 20, 5, 1),
+(2, 'Zhen vintage T-shirt', 'Kung Fu Panda 4 Zhen vintage shot Premium T-shirt', 'Slate Grey', 43, 3, 1),
+(3, 'Master Tigress T-shirt', 'Men\'s Kung Fu Panda Master Tigress Iron Fist Sketch Portrait Tee', 'White', 43, 1, 1),
+(4, 'Furious 5 T-shirt', 'Kung Fu Panda Furious Five T-Shirt', 'White', 23, 9, 1),
+(5, 'Pullover Hoodie', 'DreamWorks Kung Fu Panda 4 Kawaii Zhen Pullover Hoodie', 'Navy Blue', 54, 2, 1),
+(6, 'Hoodie', 'DreamWorks Kung Fu Panda 4 Po & Zhen Zip Hoodie', 'Navy Blue', 23, 4, 1),
+(7, 'Dumpling image Hoodie', 'Kung Fu Panda 4 Dumplings Hoodie', 'Black', 43, 3, 1),
+(8, 'panda 4 Hoodie', 'Kung Fu Panda 4 The Big City Hoodie', 'Black', 11, 4, 1),
+(9, 'Warrior Hoodie', 'Kung Fu Panda 4 The Dragon Warrior Hoodie', 'Black', 65, 5, 1),
+(10, 'Panda with wristie soft toy', 'Panda 4 Po Staff Wristie', 'white and black', 45, 1, 2),
+(11, 'cute zhen soft toy', 'Kung Fu Panda Zhen cute Plush Toy', 'multi coloured', 34, 3, 2),
+(12, 'Zhen soft toy', 'Zhen plushie', 'Beige', 24, 3, 2),
+(13, 'cute panda soft toy', 'Kung Fu Panda Po cute Plush Toy', 'white and black', 35, 2, 2),
+(14, 'Po and Tigress 2 pc set plushies', 'Great combo of warriors Po and Tigress soft Plushies', 'multi coloured', 55, 5, 2),
+(15, 'Vinyl action figure of po', 'Kung Fu Panda Po Bendyfigs Action Figure', 'white and black', 76, 3, 3),
+(16, 'Kung Fu Panda - Tigress vinyl figure', 'Kung Fu Panda Funko POP Movies Tigress Vinyl Figure', 'golden yellow', 44, 1, 3),
+(17, 'Po Mini versioFigurine', 'Miniature Panda Decor 4Pcs Mini Panda Figurine Cute Panda Doll Kung Fu PVC Panda Statue Fairy Garden Animals for Plant Pots Bonsai Craft Cake Topper Mini Panda Figurines', 'white and black', 65, 3, 3),
+(18, 'Kung Fu Panda - Po Figure', 'Funko Pop! Kung Fu Panda Flocked Po EE Exclusive', 'white and black', 54, 1, 3),
+(19, 'Kung Fu Panda - Po Action Figure', 'The Fighter Po Action Figure', 'white and black', 55, 2, 3),
+(20, 'Po key chain', 'round plastic asthetic key chain', 'Green', 76, 1, 4),
+(21, 'Dragon key chain', 'Kung Fu Panda series Dragon key chain', 'multi coloured', 48, 2, 4),
+(22, 'Ping key chain ', 'Mr.Ping Mongram key chain', ' ', 76, 1, 4),
+(23, 'Key chain', 'copper craft Kung Fu Panda Series key chain', 'copper', 34, 1, 4),
+(24, 'Tumbler with Po', 'Kung Fu Panda Dragon Warrior Tumbler Cup', 'multi coloured', 87, 2, 4),
+(25, 'warrior Tumbler', 'Kung Fu Panda the Dragon Warrior Tumbler Cup', 'multi coloured', 34, 1, 4),
+(26, 'Coffee Mug', 'Dragon Master Mug', 'multi coloured', 77, 4, 5),
+(27, 'Po Mug', 'wanna drink coffee with Po', 'white and black', 44, 5, 5),
+(28, 'Peaceful Po Mug', 'Mug with peaceful Po', 'white and black', 87, 3, 5),
+(29, 'Panda wall poster', 'Trends International Kung Fu Panda 4 - Practice Framed Wall Poster Prints', 'red with white and black', 45, 2, 5),
+(30, 'Zhen poster', 'Zhen KUNG FU PANDA 4 movie poster - Wall Art Decor Cinephile Gift', 'multi coloured', 45, 1, 5),
+(31, 'KFP 4 Poster', 'Kung Fu Panda 4 Poster', 'multi coloured', 44, 5, 5);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `purchases`
---
 
 CREATE TABLE `purchases` (
   `purchase_id` int(11) NOT NULL,
@@ -205,13 +186,6 @@ CREATE TABLE `purchases` (
   `total_purchase_cost` decimal(10,0) DEFAULT NULL,
   `purchase_method` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `purchases`
---
-
-INSERT INTO `purchases` (`purchase_id`, `customer_id`, `total_purchase_cost`, `purchase_method`) VALUES
-(6, 1, 220, 'card');
 
 -- --------------------------------------------------------
 
@@ -225,13 +199,6 @@ CREATE TABLE `purchase_product` (
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `purchase_product`
---
-
-INSERT INTO `purchase_product` (`purchase_product_id`, `purchase_id`, `product_id`, `quantity`) VALUES
-(4, 6, 11, 5);
 
 --
 -- Indexes for dumped tables
@@ -306,7 +273,7 @@ ALTER TABLE `purchase_product`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `category`
@@ -342,19 +309,19 @@ ALTER TABLE `login_details`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `purchases`
 --
 ALTER TABLE `purchases`
-  MODIFY `purchase_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `purchase_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `purchase_product`
 --
 ALTER TABLE `purchase_product`
-  MODIFY `purchase_product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `purchase_product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
